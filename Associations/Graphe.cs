469344e178881fb29eb2e4c30f890d8b8e99e275
lecroissantdu-06 <trgraham@orange.fr>
2025-03-01 16:45:32 +0100

    Update Graphe.cs

--- Associations/Graphe.cs
+++ Associations/Graphe.cs
@@ -1,5 +1,6 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Data;
 using System.Linq;
 using System.Text;
@@ -11,6 +12,8 @@ namespace Associations
     {
         public Noeud[] sommets; //liste des sommets
         public Lien[] liens; //liste des arrêtes
+        public Dictionary<Noeud, List<Noeud>> ListeAdj;
+
 
         public Graphe(string filename)
         {
@@ -30,7 +33,7 @@ namespace Associations
                     i++;
                 }
             }
-            
+
             Noeud[] listeN = new Noeud[Int32.Parse(lines[i].Substring(0, lines[i].IndexOf(' ')))];
             Lien[] listeL = new Lien[lines.Length - i - 1];
 
@@ -42,20 +45,22 @@ namespace Associations
 
             i++;
             int relation = i;   //ici, lines[relation] est la première relation
-            
+
 
             while (relation < lines.Length)
             {
-                listeL[relation-i] = new Lien(Sommets[Convert.ToInt32(lines[relation].Split(' ')[0])-1], Sommets[Convert.ToInt32(lines[relation].Split(' ')[1])-1]);
+                listeL[relation - i] = new Lien(Sommets[Convert.ToInt32(lines[relation].Split(' ')[0]) - 1], Sommets[Convert.ToInt32(lines[relation].Split(' ')[1]) - 1]);
                 relation++;
             }
             Liens = listeL;
+
+            this.ListeAdj = GenererListeAdjacence();
 
             AfficheSommets();
             AfficheLiens();
         }
 
-#region Proprietes
+        #region Proprietes
         public Noeud[] Sommets
         {
             get { return sommets; }
@@ -68,31 +73,34 @@ namespace Associations
             get { return liens; }
             set { liens = value; }
         }
-#endregion
+        #endregion
 
 
-        public Dictionary<Noeud,List<Noeud>> GenererListeAdjacence(){
-            Dictionary<Noeud,List<Noeud>> lAdjacence = new Dictionary<Noeud,List<Noeud>>();
+        public Dictionary<Noeud, List<Noeud>> GenererListeAdjacence()
+        {
+            Dictionary<Noeud, List<Noeud>> lAdjacence = new Dictionary<Noeud, List<Noeud>>();
             List<Noeud> noeudsAdjacents = new List<Noeud>();
-            for (int i = 0; i<sommets.Length;i++)
+            for (int i = 0; i < sommets.Length; i++)
             {
                 foreach (Lien l in liens)
                 {
-                    if(l.n1 == sommets[i])
+                    if (l.n1 == sommets[i])
                     {
                         noeudsAdjacents.Add(l.n2);
                     }
-                    else if(l.n2 == sommets[i])
+                    else if (l.n2 == sommets[i])
                     {
                         noeudsAdjacents.Add(l.n1);
                     }
                 }
-                lAdjacence.Add(sommets[i],noeudsAdjacents);
+                lAdjacence.Add(sommets[i], noeudsAdjacents.ToList());
+
                 noeudsAdjacents.Clear();
+                
             }
 
-             return lAdjacence;
-            }
+            return lAdjacence;
+        }
 
 
         public bool NoeudExiste()
@@ -123,5 +131,75 @@ namespace Associations
                 Console.WriteLine();
             }
         }
+
+        public int[][] GenererMatriceAdjacence()
+        {
+            int[][] mAdj = new int[this.Sommets.Length][];
+            for (int i = 0; i < this.Sommets.Length; i++)
+            {
+                int[] ligneActuelle = new int[this.Sommets.Length];
+                for (int j = 0; j < this.Sommets.Length; j++)
+                {
+                    ligneActuelle[j] = 0;
+                }
+                mAdj[i] = ligneActuelle;
+            }
+
+            foreach (Lien l in liens)
+            {
+                int sommet1Index = l.n1.num-1; //-1 car sommets commencent à 1
+                int sommet2Index = l.n2.num-1;
+                mAdj[sommet1Index][sommet2Index] = 1;
+                mAdj[sommet2Index][sommet1Index] = 1;
+            }
+
+            return mAdj;
+        }
+
+        public List<Noeud> ParcoursEnLargeur(Noeud NoeudDepart)
+        {
+            List<Noeud> Visitees = new List<Noeud>();
+            Queue<Noeud> fileNoeuds = new Queue<Noeud>();
+            Visitees.Add(NoeudDepart);
+            fileNoeuds.Enqueue(NoeudDepart);
+
+            while (fileNoeuds.Count > 0) 
+            { 
+                Noeud element = fileNoeuds.Dequeue();
+                foreach (Noeud n in this.ListeAdj[element])
+                {
+                    if (!Visitees.Contains(n))
+                    {
+                        fileNoeuds.Enqueue(n);
+                        Visitees.Add(n);
+                    }
+                }
+            }
+            return Visitees;
+        }
+        public List<Noeud> ParcoursEnProfondeur(Noeud NoeudDepart)
+        {
+            List<Noeud> Visitees = new List<Noeud>();
+            Stack<Noeud> pileNoeuds = new Stack<Noeud>();
+            Visitees.Add(NoeudDepart);
+            pileNoeuds.Push(NoeudDepart);
+
+            while (pileNoeuds.Count > 0)
+            {
+                Noeud element = pileNoeuds.Pop();
+                foreach (Noeud n in this.ListeAdj[element])
+                {
+                    if (!Visitees.Contains(n))
+                    {
+                        pileNoeuds.Push(n);
+                        Visitees.Add(n);
+                    }
+                }
+            }
+            return Visitees;
+        }
+
+
+
     }
 }
