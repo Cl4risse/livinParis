af5d5c4481c27bb7a32a9b89427f44f233b2f631
lecroissantdu-06 <trgraham@orange.fr>
2025-03-01 21:13:35 +0100

    Update Noeud.cs

--- Associations/Noeud.cs
+++ Associations/Noeud.cs
@@ -1,26 +1,34 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using SkiaSharp;
 
 namespace Associations
 {
     internal class Noeud
     {
         public int num;
+        public int x;
+        public int y;
         public Noeud() { }
         public Noeud(int b) { this.num = b; }
-        public Noeud(string a) 
-        { 
+        public Noeud(string a)
+        {
             this.num = Int32.Parse(a);
         }
 
+        public void AjouterCoords(int x, int y)
+        {
+            this.x = x;
+            this.y = y;
+        }
 
         public void AfficheN()
         {
             Console.Write(num);
-            //test
         }
+
     }
 }
